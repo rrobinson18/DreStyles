fbf380d0b3f7ac35030e1eba6f8062c200e66820
rrobinson18 <robinson.rakesha@gmail.com>
2019-04-07 17:03:04 -0400

    Nav, Sections, Footer Updated

--- home.js
+++ home.js
@@ -1,26 +1,97 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+//******Carousel********/ 
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+$(document).ready(function () {
+    var carousel = $("#carousel").waterwheelCarousel({
+      flankingItems: 3,
+      movingToCenter: function ($item) {
+        $('#callback-output').prepend('movingToCenter: ' + $item.attr('id') + '<br/>');
+      },
+      movedToCenter: function ($item) {
+        $('#callback-output').prepend('movedToCenter: ' + $item.attr('id') + '<br/>');
+      },
+      movingFromCenter: function ($item) {
+        $('#callback-output').prepend('movingFromCenter: ' + $item.attr('id') + '<br/>');
+      },
+      movedFromCenter: function ($item) {
+        $('#callback-output').prepend('movedFromCenter: ' + $item.attr('id') + '<br/>');
+      },
+      clickedCenter: function ($item) {
+        $('#callback-output').prepend('clickedCenter: ' + $item.attr('id') + '<br/>');
+      }
+    });
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+    $('#prev').bind('click', function () {
+      carousel.prev();
+      return false
+    });
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
+    $('#next').bind('click', function () {
+      carousel.next();
+      return false;
+    });
+
+    $('#reload').bind('click', function () {
+      newOptions = eval("(" + $('#newoptions').val() + ")");
+      carousel.reload(newOptions);
+      return false;
+
+
+      
+    });
+
+  });
+
+  function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+
+  //*******Testimonial******/
+  
+  let slideIndex = 1;
+  showSlides(slideIndex);
+
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName('myReviews');
+    let dots = document.getElementsByClassName('dot');
+    if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+      slides[i].style.display = 'none';
     }
     for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+      dots[i].className = dots[i].className.replace('active', '');
     }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+    slides[slideIndex-1].style.display = 'block';
+    dots[slideIndex-1].className += ' active';
+  }
+
+ 
+
+  //*****Nav Font Color Change******/
+
+  let header = $('header'),
+  minScroll = 50;
+
+  $(document).scroll(function () {
+    let scrollTop = $(this).scrollTop();
+
+    if (scrollTop > minScroll) {
+      header.addClass('compact');
+    }
+    else {
+      header.removeClass('compact');
+    }
+  });
--- portfolio/portfolio.js
+++
@@ -1,8 +0,0 @@
-function bigImg(x) {
-  x.style.height = "600px";
-  x.style.width = "500px";
-}
-function normalImg(x) {
-  x.style.height = "500px";
-  x.style.width = "450px";
-}
